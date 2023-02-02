61b20485d39a256b8746b39755a53dcd1420ab95
mck06016 <mck06016@buyi.edu>
2023-02-02 15:37:32 -0800

    push failed 2.1 - re sending

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction one = new Fraction();
+        Console.WriteLine(one.GetFractionString());
+        Console.WriteLine(one.GetDecimalValue());
+
+        Fraction two = new Fraction(5);
+        Console.WriteLine(two.GetFractionString());
+        Console.WriteLine(two.GetDecimalValue());
+
+        Fraction three = new Fraction(3,4);
+        Console.WriteLine(three.GetFractionString());
+        Console.WriteLine(three.GetDecimalValue());
+
+        Fraction four = new Fraction(1,3);
+        Console.WriteLine(four.GetFractionString());
+        Console.WriteLine(four.GetDecimalValue());
+
     }
 }
